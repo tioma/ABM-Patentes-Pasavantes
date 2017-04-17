31924f7f736949dc0e7ee1b03221538558363640
Artiom Kolesnikov <artiom.kolesnikov@gmail.com>
2017-04-17 13:51:56 -0300

    Corrección server_url

--- administrador-pasavantes/config.app.js
+++ administrador-pasavantes/config.app.js
@@ -2,8 +2,8 @@
  * Created by Artiom on 4/10/16.
  */
 administradorPasavantes.constant('APP_CONFIG', {
-	//SERVER_URL: 'http://terminales.puertobuenosaires.gob.ar:8099',
+	SERVER_URL: 'http://terminales.puertobuenosaires.gob.ar:8099',
 	//SERVER_URL: 'http://localhost:8099',
-	SERVER_URL: 'http://10.10.0.223:8099',
+	//SERVER_URL: 'http://10.10.0.223:8099',
 	API_ENDPOINT: 'http://www.e-puertobue.com.ar/ws'
 });
